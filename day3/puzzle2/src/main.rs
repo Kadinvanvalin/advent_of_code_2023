a2042cade49637448bc126b980be52243fd31335
kadin van valin <kadin@kadin.codes>
2023-12-04 21:10:09 -0600

    [main] broken

--- day3/puzzle2/src/main.rs
+++ day3/puzzle2/src/main.rs
@@ -9,31 +9,34 @@ mod tests {
     #[test]
     fn test_one() {
        let schematic: Vec<String> = vec![
-            "467..11489".to_string(),
-			"......*...".to_string(),
+            "467...5...".to_string(),
+            "*.....*...".to_string(),
+            ".....10..9".to_string(),
         ];
         
-        let valid_digits = read_schematic(schematic);
-        assert_eq!(count(valid_digits), 11489);
+        let found_tokens = read_schematic(schematic);
+   let numbers = flatten(found_tokens.clone());
+    let count = count(numbers);
+        assert_eq!(count, 50);
     }
-    #[test]
-    fn test_two() {
-       let schematic: Vec<String> = vec![
-            "467..114..".to_string(),
-            "...*......".to_string(),
-            "..35..633.".to_string(),
-		"......#...".to_string(),
-		"617*......".to_string(),
-		".....+.58.".to_string(),
-		"..592.....".to_string(),
-		"......755.".to_string(),
-		"...$.*....".to_string(),
-		".664.598..".to_string(),
-        ];
-        
-        let valid_digits = read_schematic(schematic);
-        assert_eq!(count(valid_digits), 451490);
-    }
+//    #[test]
+//    fn test_two() {
+//       let schematic: Vec<String> = vec![
+//            "467..114..".to_string(),
+//            "...*......".to_string(),
+//            "..35..633.".to_string(),
+//            "......#...".to_string(),
+//            "617*......".to_string(),
+//            ".....+.58.".to_string(),
+//            "..592.....".to_string(),
+//            "......755.".to_string(),
+//            "...$.*....".to_string(),
+//            ".664.598..".to_string(),
+//        ];
+//        
+//        let valid_digits = read_schematic(schematic);
+//        assert_eq!(count(valid_digits), 451490);
+//    }
 }
     const DIRECTIONS: &'static [(i32, i32)] = &[
         (-1,-1),
@@ -49,6 +52,22 @@ mod tests {
 #[derive(Clone)]
 struct Digit {
     y: i32,
+    x: i32,
+    value: String,
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
+struct Token {
+    y: i32,
+    x: i32,
+    digits: Vec<Digit>,
+}
+
+#[derive(Debug)]
+#[derive(Clone)]
+struct Number {
+    y: i32,
     start_x: i32,
     end_x: i32,
     value: String,
@@ -63,72 +82,69 @@ fn main() {
             }
         }
     }
-    let valid_digits: Vec<Digit> = read_schematic(schematic);
-    
-    println!("{}", count(valid_digits));
+    let found_tokens: Vec<Token> = read_schematic(schematic);
+   let numbers = flatten(found_tokens.clone());
+    let count = count(numbers);
+    println!("{:?}", found_tokens);
+    println!("{:?}", count);
 }
-fn count(valid_digits: Vec<Digit>) -> i32 {
-   let mut count = 0;
-   println!("ALL VALID DIGIGITS COUNTING {:?}", valid_digits);
-   for digit in &valid_digits {
-        println!("valid digit:: {:?}", digit);
-        count += digit.value.parse::<i32>().unwrap();
-        println!("current count: {}", count);
+fn count (numbers: Vec<Number>) -> i32 {
+    let mut count = 0;
+    for number in numbers {
+        let num: i32 = number.value.parse().unwrap();
+        count += num;
     }
-    return count;
+    return count
 }
-fn read_schematic(schematic: Vec<String>) -> Vec<Digit> {
-    let mut found_digits: Vec<Digit> = vec![];
-    let mut valid_digits: Vec<Digit> = vec![];
+fn flatten(found_tokens: Vec<Token>) -> Vec<Number> {
+   let mut flattened_numbers: Vec<Number> = vec![];
+    let mut number: Number = Number {
+       y: -1,
+        start_x: -1,
+        end_x: -1,
+        value: String::new(),
+    };
+   for token in &found_tokens {
+        for digit in &token.digits {
+        if number.y == -1 {
+                number.y = digit.y;
+                number.start_x = digit.x;
+                number.end_x = digit.x;
+                number.value.push_str(&digit.value);
+        }
+        if digit.x + 1 == number.end_x {
+                number.end_x = digit.x;
+                number.value.push_str(&digit.value);
+         }
+        flattened_numbers.push(number.clone());
+        number = Number {
+       y: -1,
+        start_x: -1,
+        end_x: -1,
+        value: String::new(),
+    };
+    }
+   }
+    return flattened_numbers;
+}
+fn read_schematic(schematic: Vec<String>) -> Vec<Token> {
+    let mut found_tokens: Vec<Token> = vec![];
     for (y, line) in schematic.clone().iter().enumerate() {
         let y: i32 = y.try_into().unwrap();
-        let mut digit = Digit { y, start_x: -1, end_x: -1, value: String::new() };
+        // let mut digit = Digit { y, start_x: -1, end_x: -1, value: String::new() };
         for (x, char) in line.chars().enumerate() {
         let x = x.try_into().unwrap();
-            if char.is_digit(10) {
-               if digit.start_x == -1 {
-                    digit.start_x = x;
-               } 
-               digit.value.push(char);
-                if x == <usize as TryInto<i32>>::try_into(line.len()).unwrap()  - 1 {
-
-                 digit.end_x = x;
-                 let complete_digit = Digit {
-                    y,
-                    start_x: digit.start_x,
-                    end_x: digit.end_x,
-                    value: digit.value,
-                };
-                    println!("complete_digit:: {:?}", complete_digit);
-                found_digits.push(complete_digit);
-                digit.start_x = -1;
-                digit.end_x =  -1;
-                digit.value = String::new();
-                }
-            } else {
-               if !(digit.start_x == -1) {
-                 digit.end_x = x;
-                 let complete_digit = Digit {
-                    y,
-                    start_x: digit.start_x,
-                    end_x: digit.end_x,
-                    value: digit.value,
-                };
-                    println!("complete_digit:: {:?}", complete_digit);
-                found_digits.push(complete_digit);
-                digit.start_x = -1;
-                digit.end_x =  -1;
-                digit.value = String::new();
+            if char == '*' {
+                found_tokens.push(Token { y, x, digits: vec![] });
                }
             }
         }
-        }
+        
     
-    println!("ALL DIGITS::: {:?}", found_digits);
-     for digit in &found_digits {
-         'outer: for x in digit.start_x..digit.end_x {
+     for token in &mut found_tokens {
+         for x in token.x..token.x {
             println!("looking for digits:: x:: {}",x);
-            let y = digit.y;
+            let y = token.y;
             for dir in DIRECTIONS {
             let (y_diff, x_diff) = dir;
                 let y = y + y_diff; 
@@ -137,32 +153,31 @@ fn read_schematic(schematic: Vec<String>) -> Vec<Digit> {
                 if y < 0 || x < 0 {
                     println!("out of bounds, skipping {}, {}", y,x);
                 } else {
-                println!("digit:: {:?}, x:: {}, y:: {}", digit, x, y);
+                //println!("digit:: {:?}, x:: {}, y:: {}", digit, x, y);
                 if y < schematic.len() as i32 {
                     let y = y as usize; 
                     let string = &schematic[y];
                     if x < string.len().try_into().unwrap() {
                         let char = &string.chars().nth(x.try_into().unwrap()).unwrap();
-                                println!("looking for match for digit:: {:?} FOUND CHAR {}", digit, char);
-                        if !char.is_digit(10) && char != &'.' {
-                                println!("found match for digit:: {:?}", digit);
-                             let complete_digit = Digit {
-                                y: digit.y,
-                                start_x: digit.start_x,
-                                end_x: digit.end_x,
-                                value: digit.value.clone(),
+                        if char.is_digit(10) {
+                             let digit = Digit {
+                                y: y as i32,
+                                x,
+                                value: char.to_string(),
                             };
-                           valid_digits.push(complete_digit);
-                           break 'outer; 
+                           token.digits.push(digit);
                         }
                     }
                 } 
                 }
             }
+        
         }
     }
-    return valid_digits;
+    return found_tokens;
 }
+
+
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> 
     where P: AsRef<Path>, {
     let file = File::open(filename)?;
